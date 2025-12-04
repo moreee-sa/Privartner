755e5158e2d3468d10cee8c61fbadb5905bda89a
moreee-sa <morenolms41@gmail.com>
2025-12-04 20:49:10 +0100

    Aggiunta generazione chiave AES-GCM e visualizzazione in React con fallback

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,46 @@
-function Home() {
-  return (
-    <div className="bg-amber-400">
-      <h1 className="text-white">Privartner</h1>
-    </div>
-  )
+import { useState, useEffect } from "react";
+
+async function exportCryptoKey(key: CryptoKey) {
+  const jwk = await window.crypto.subtle.exportKey("jwk", key);
+  return JSON.stringify(jwk);
 }
 
-export default Home
+async function generaChiave() {
+  const key = await window.crypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"],
+  );
+
+  return key
+}
+
+function Home() {
+  const [chiave, setChiave] = useState<string | null>();
+
+  useEffect(() => {
+    async function init() {
+      const cryptoKey  = await generaChiave();
+      const jwkString = await exportCryptoKey(cryptoKey);
+      const jwk = JSON.parse(jwkString);
+      const key = jwk.k;
+      setChiave(key);
+    }
+
+    init();
+  }, []);
+
+  return (
+    <div className="bg-black">
+      <h1 className="text-white">Privartner</h1>
+      <span className="text-white">
+        key: {chiave ?? "Generazione chiave in corso..."}
+      </span>
+    </div>
+  );
+}
+
+export default Home;
