a490638673bec759eca6c8a5ea089bc1bdcd5f10
moreee-sa <morenolms41@gmail.com>
2025-12-05 00:59:30 +0100

    Creato componente Home con generazione chiave e integrazione KeyDisplay e AddContact

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,42 @@
 import { useState, useEffect } from "react";
 import { generaChiave, esportaCryptoKey } from "@/lib/crypto";
+import { THEME } from "@/lib/constants";
+import Navbar from "@/components/Navbar";
+import KeyDisplay from "@/components/Home/KeyDisplay";
+import AddContact from "@/components/Home/AddContact";
+
+async function init() {
+  const cryptoKey  = await generaChiave();
+  const jwkString = await esportaCryptoKey(cryptoKey);
+  const jwk = JSON.parse(jwkString);
+  const key = jwk.k;
+  return key;
+}
 
 function Home() {
   const [chiave, setChiave] = useState<string | null>();
 
   useEffect(() => {
-    async function init() {
-      const cryptoKey  = await generaChiave();
-      const jwkString = await esportaCryptoKey(cryptoKey);
-      const jwk = JSON.parse(jwkString);
-      const key = jwk.k;
+    async function load() {
+      const key = await init();
       setChiave(key);
     }
-
-    init();
+    load();
   }, []);
 
+  async function handleClick() {
+    const key = await init();
+    setChiave(key);
+  }
+
   return (
-    <div className="bg-black">
-      <h1 className="text-white">Privartner</h1>
-      <span className="text-white">
-        key: {chiave ?? "Generazione chiave in corso..."}
-      </span>
+    <div className="min-h-screen" style={{ backgroundColor: THEME.background }}>
+      <Navbar />
+      <div className="flex flex-col gap-5 px-10 lg:px-52">
+        <KeyDisplay chiave={chiave} handleClick={handleClick} />
+        <AddContact chiave={chiave} />
+      </div>
+      
     </div>
   );
 }
