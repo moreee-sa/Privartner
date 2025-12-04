9a300190b91f2dc991ace11d50ac42c0734d9674
moreee-sa <morenolms41@gmail.com>
2025-12-05 00:58:07 +0100

    Creata Navbar come componente separato

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router"
+import { THEME } from "@/lib/constants"
+
+function Navbar() {
+  return (
+    <div className="w-full py-5 px-10 lg:px-52">
+      <Link
+        to={"#"}
+        className="text-2xl"
+        style={{ color: THEME.text }}
+      >
+        Privartner
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
