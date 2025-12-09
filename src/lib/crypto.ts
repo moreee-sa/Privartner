ac58893ac6542e22ed0d204d1b3768962e00696c
moreee-sa <morenolms41@gmail.com>
2025-12-09 15:26:09 +0100

    Sostituito vecchio metodo di generazione chiavi con ECDSA P-384 e migliorata l’estrazione della chiave pubblica

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,17 +1,36 @@
-export async function generaChiave() {
-  const key = await window.crypto.subtle.generateKey(
+export async function generaCoppiaChiavi() {
+  const keyPair = await window.crypto.subtle.generateKey(
     {
-      name: "AES-GCM",
-      length: 256,
+      name: "ECDSA",
+      namedCurve: "P-384",
     },
     true,
-    ["encrypt", "decrypt"],
+    ["sign", "verify"],
   );
 
-  return key
+  return keyPair
 }
 
-export async function esportaCryptoKey(key: CryptoKey) {
-  const jwk = await window.crypto.subtle.exportKey("jwk", key);
-  return JSON.stringify(jwk);
+export async function esportaCryptoKeyPair(keyPair: CryptoKeyPair) {
+  const pubJwk = await window.crypto.subtle.exportKey("jwk", keyPair.publicKey);
+  const privJwk = await window.crypto.subtle.exportKey("jwk", keyPair.privateKey);
+
+  return JSON.stringify({ publicKey: pubJwk, privateKey: privJwk });
+}
+
+export async function getPublicKeyStringXY(jwkPair: string) {
+  const jwkjson = JSON.parse(jwkPair);
+  const publicKeyX = jwkjson.publicKey.x;
+  const publicKeyY = jwkjson.publicKey.y;
+
+  return publicKeyX + publicKeyY;
+}
+
+export async function saveKeyPair(keyPair: CryptoKeyPair) {
+  const pubJwk = await crypto.subtle.exportKey("jwk", keyPair.publicKey);
+  const privJwk = await crypto.subtle.exportKey("jwk", keyPair.privateKey);
+
+  const jwkPair = { publicKey: pubJwk, privateKey: privJwk };
+  localStorage.setItem("lastPairKey", JSON.stringify(jwkPair));
+  console.log("Coppia di chiavi salvate");
 }
