10b2211a79177d89ad31501ae9b77bcedb03713f
moreee-sa <morenolms41@gmail.com>
2025-12-10 18:23:45 +0100

    Implementati componenti nella pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { generaCoppiaChiavi, esportaCryptoKeyPair, getPublicKeyStringRSA, saveKeyPair} from "@/lib/crypto";
+import { loadContacts } from "@/lib/contacts";
 import { THEME } from "@/lib/constants";
 import Navbar from "@/components/Navbar";
 import KeyDisplay from "@/components/Home/KeyDisplay";
 import AddContact from "@/components/Home/AddContact";
+import ContactList from "@/components/Contact";
+import type { Contact } from "@/lib/contacts";
 
 async function init() {
   const cryptoKeyPair = await generaCoppiaChiavi();
@@ -16,14 +19,16 @@ async function init() {
 
 function HomePage() {
   const [chiave, setChiave] = useState<string | null>(null);
+  const [contatti, setContatti] = useState<Contact[] | null>(null);
 
   useEffect(() => {
     async function load() {
+      const contacts = loadContacts();
+      setContatti(contacts)
       const { encodedKey, cryptoKeyPair } = await init();
       setChiave(encodedKey);
       await saveKeyPair(cryptoKeyPair);
     }
-    
     load();
   }, []);
 
@@ -40,6 +45,7 @@ function HomePage() {
       <div className="flex flex-col gap-5 px-10 lg:px-52">
         <KeyDisplay chiave={chiave} handleClick={generateNewKeyPair} />
         <AddContact />
+        <ContactList contacts={contatti} />
       </div>
     </div>
   );
