1d3c28dec792ba3c96457267395174579aa18b24
moreee-sa <morenolms41@gmail.com>
2025-12-10 18:33:00 +0100

    Aggiunta route dinamica /contact/:id per ContactPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router'
 import Home from '@pages/Home'
-import AddContactPage from './pages/Add'
+import AddContactPage from '@pages/Add'
+import ContactPage from '@pages/Contact'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/add' element={<AddContactPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/contact/:id' element={<ContactPage />} />
       </Routes>
     </HashRouter>
   </StrictMode>,
