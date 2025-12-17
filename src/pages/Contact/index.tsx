05c9ed57fd3ffba5f48213eb4e49721c3c84f873
moreee-sa <morenolms41@gmail.com>
2025-12-17 17:27:46 +0100

    Aggiunta struttura della pagina per visualizzare il contatto

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,14 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useParams, useNavigate } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import type { Contact } from "@/lib/contacts";
 
 function ContactPage() {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
 
+  const [contact, setContact] = useState<Contact | null>(null);
+  const [showError, setShowError] = useState<boolean>(false)
+
   useEffect(() => {
     if (!id) {
       navigate("/");
+      return;
     }
+
+    async function load() {
+      const contactsString = localStorage.getItem("contacts");
+      
+      if (!contactsString) {
+        return;
+      }
+    
+      const contacts: Contact[] = JSON.parse(contactsString);
+      const foundContact = contacts.find(c => c.id === id);
+      if (foundContact) {
+        setContact(foundContact);
+      }else{setShowError(true)}
+    }
+
+    load()
   }, [id, navigate]);
 
   if (!id) return null
@@ -16,7 +38,10 @@ function ContactPage() {
   return (
     <>
       <h1>Pagina Contatti</h1>
-      <span>{id}</span>
+      {showError && <h1>Contatto non trovato</h1>}
+      <p>{id}</p>
+      <p>{contact?.name}</p>
+      <p>{contact?.description}</p>
     </>
   )
 }
