7e7093a0f12e7ca182ca2c29f9fa3dd1a993ffe7
moreee-sa <morenolms41@gmail.com>
2025-12-27 14:43:10 +0100

    Implementato componente ContactCard

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,6 @@
 import type { Contact } from "@/lib/contacts"
-import { MdAccountCircle } from "react-icons/md";
 import { THEME } from "@/lib/constants";
-import { Link } from "react-router";
+import ContactCard from "./ContactCard";
 
 interface ContactsProps {
   contacts: Contact[] | null;
@@ -16,22 +15,7 @@ function ContactList({ contacts }: ContactsProps) {
       <div className="flex flex-col gap-2 pt-5">
         {contacts && contacts.length > 0 ? (
           contacts.map(contact => (
-            <Link key={contact.id} to={`/contact/${contact.id}`}>
-              <div
-                className="flex items-center justify-start gap-4 p-5 rounded-lg opacity-80 hover:opacity-100 transition-opacity"
-                style={{ backgroundColor: THEME.button }}
-              >
-                <MdAccountCircle size={30} color={THEME.text} />
-                <div className="flex flex-col">
-                  <span className="text-base md:text-lg lg:text-xl" style={{ color: THEME.text }}>
-                    {contact.name}
-                  </span>
-                  <span className="text-xs md:text-base" style={{ color: THEME.textSecondary }}>
-                    {contact.description}
-                  </span>
-                </div>
-              </div>
-            </Link>
+            <ContactCard contact={contact} />
           ))
         ) : (
           <p style={{ color: THEME.text }}>
