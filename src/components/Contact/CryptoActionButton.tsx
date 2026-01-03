9663e2816407d6478729a87b397202f9465e7b2a
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-01-04 00:22:59 +0100

    Aggiunto prop per gestire lo stato del pulsante quando non è possibile eseguire operazioni crittografiche

--- src/components/Contact/CryptoActionButton.tsx
+++ src/components/Contact/CryptoActionButton.tsx
@@ -2,15 +2,17 @@ import { THEME } from "@/lib/constants";
 
 interface buttonProps {
   actionText: string;
+  disableButton: boolean;
   handleClick?: () => void;
 }
 
-function CryptoActionButton({ actionText, handleClick}: buttonProps ) {
+function CryptoActionButton({ actionText, disableButton, handleClick}: buttonProps ) {
   return (
     <button
-      className="rounded-lg w-full h-16 flex items-center justify-center gap-5 opacity-80 hover:opacity-100 transition-opacity cursor-pointer"
+      className={`rounded-lg w-full h-16 flex items-center justify-center ${disableButton ? "opacity-50 cursor-not-allowed" : "opacity-80 hover:opacity-100 cursor-pointer"} gap-5  transition-opacity`}
       style={{ backgroundColor: THEME.button }}
       onClick={handleClick}
+      disabled={disableButton}
     >
       <span
         className="text-base md:text-lg lg:text-xl"
