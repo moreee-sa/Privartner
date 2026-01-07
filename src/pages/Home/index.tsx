65c3dab4234c2bcb526a4f69d977fb1904171b4a
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-01-07 16:58:07 +0100

    Home: refactor per usare Redux invece di localStorage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,25 @@
+// Librerie esterne
 import { useState, useEffect } from "react";
-import { generaCoppiaChiavi, esportaCryptoKeyPair, getPublicKeyStringRSA, saveKeyPair} from "@/lib/crypto";
+import { useDispatch } from "react-redux";
+import '@fontsource-variable/nunito-sans';
+
+// Moduli interni
+import { generaCoppiaChiavi, esportaCryptoKeyPair, getPublicKeyStringRSA } from "@/lib/crypto";
 import { loadContacts } from "@/lib/contacts";
 import { THEME } from "@/lib/constants";
+
+// Componenti
 import Navbar from "@/components/Navbar";
 import KeyDisplay from "@/components/Home/KeyDisplay";
 import AddContact from "@/components/Home/AddContact";
 import ContactList from "@/components/Contact";
+
+// Tipi
 import type { Contact } from "@/lib/contacts";
-import '@fontsource-variable/nunito-sans';
+import type { AppDispatch } from "@/state/store";
+
+// Redux slices / actions
+import { setKeyPair } from "@/state/keyPair/keyPairSlice";
 
 async function init() {
   const cryptoKeyPair = await generaCoppiaChiavi();
@@ -19,25 +31,61 @@ async function init() {
 }
 
 function HomePage() {
-  const [chiave, setChiave] = useState<string | null>(null);
-  const [contatti, setContatti] = useState<Contact[] | null>(null);
+  const [publicKeyPreview, setPublicKeyPreview] = useState<string | null>(null);
+  const [contacts, setContacts] = useState<Contact[] | null>(null);
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    async function load() {
-      const contacts = loadContacts();
-      setContatti(contacts)
-      const { encodedKey, cryptoKeyPair } = await init();
-      setChiave(encodedKey);
-      await saveKeyPair(cryptoKeyPair);
+    const load = async () => {
+      try {
+        // Carica contatti
+        const contacts = loadContacts();
+        setContacts(contacts)
+  
+        // Genera chiavi
+        const { encodedKey, cryptoKeyPair } = await init();
+
+        // Esporta chiavi
+        // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/exportKey
+        const pubJwk = await window.crypto.subtle.exportKey("jwk", cryptoKeyPair.publicKey);
+        const privJwk = await window.crypto.subtle.exportKey("jwk", cryptoKeyPair.privateKey);
+        dispatch(setKeyPair({ 
+          publicKey: pubJwk,
+          privateKey: privJwk,
+        }));
+        
+        // Imposta chiave da visualizzare
+        setPublicKeyPreview(encodedKey);
+      } catch (err) {
+        console.error("Errore:", err);
+      }
     }
+
     load();
-  }, []);
+  }, [dispatch]);
 
   async function generateNewKeyPair() {
-    setChiave(null);
-    const { encodedKey, cryptoKeyPair } = await init();
-    setChiave(encodedKey);
-    await saveKeyPair(cryptoKeyPair);
+    try {
+      // Resetta la chiave
+      setPublicKeyPreview(null);
+
+      // Genera chiavi
+      const { encodedKey, cryptoKeyPair } = await init();
+
+      // Esporta chiavi
+      // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/exportKey
+      const pubJwk = await window.crypto.subtle.exportKey("jwk", cryptoKeyPair.publicKey);
+      const privJwk = await window.crypto.subtle.exportKey("jwk", cryptoKeyPair.privateKey);
+      dispatch(setKeyPair({ 
+        publicKey: pubJwk,
+        privateKey: privJwk,
+      }));
+
+      // Imposta chiave da visualizzare
+      setPublicKeyPreview(encodedKey);
+    } catch (err) {
+      console.error("Errore:", err)
+    }
   }
 
   return (
@@ -48,9 +96,9 @@ function HomePage() {
       <div className="w-full md:w-150 lg:w-200">
         <Navbar />
         <div className="flex flex-col gap-5 lg:gap-8 lg:items-center">
-          <KeyDisplay chiave={chiave} handleClick={generateNewKeyPair} />
+          <KeyDisplay chiave={publicKeyPreview} handleClick={generateNewKeyPair} />
           <AddContact />
-          <ContactList contacts={contatti} />
+          <ContactList contacts={contacts} />
         </div>
       </div>
     </div>
