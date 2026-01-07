d7862b2f2818c63b8dcfeca3de9c94d1308190e6
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-01-07 16:57:11 +0100

    Aggiunto Provider e store di redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router'
-import Home from '@pages/Home'
-import AddContactPage from '@pages/Add'
-import ContactPage from '@pages/Contact'
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router';
+import Home from '@pages/Home';
+import AddContactPage from '@pages/Add';
+import ContactPage from '@pages/Contact';
+import { Provider } from 'react-redux';
+import { store } from './state/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/add' element={<AddContactPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/contact/:id' element={<ContactPage />} />
-      </Routes>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/add' element={<AddContactPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/contact/:id' element={<ContactPage />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
   </StrictMode>
 )
