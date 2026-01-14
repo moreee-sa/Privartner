c2e50f407ff4b114ae7e77c8ec5ed74668aab7aa
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-01-14 18:19:39 +0100

    Migliorata ui header per mostrare il nome del contatto e il suo id

--- src/components/Contact/ContactHeader.tsx
+++ src/components/Contact/ContactHeader.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { THEME } from "@/lib/constants";
+import { MdAccountCircle } from "react-icons/md";
 
 interface ContactInfo {
   name: string | undefined;
@@ -11,30 +12,38 @@ function ContactHeader({ name, description, id }: ContactInfo) {
   const [showId, setShowId] = useState(false);
 
   return (
-    <div className="flex flex-col">
-      <span
-        className="text-2xl"
-        style={{
-          color: THEME.text,
-          fontFamily: "'Nunito Sans Variable', sans-serif",
-        }}
-      >
-        {name}
-      </span>
-      <span
-        className="text-base"
-        style={{
-          color: THEME.textSecondary,
-          fontFamily: "'Nunito Sans Variable', sans-serif",
-        }}
-        onClick={() => setShowId(prev => !prev)}
-      >
-        {showId ? (
-          id
-        ) : (
-          description
-        )}
-      </span>
+    <div
+      className="flex flex-row  p-5 rounded-2xl gap-2"
+      style={{
+        backgroundColor: THEME.button,
+      }}
+      onClick={() => setShowId(prev => !prev)}
+    >
+      <MdAccountCircle size={45} color={THEME.text} />
+      <div className="flex flex-col">
+        <span
+          className="text-2xl"
+          style={{
+            color: THEME.text,
+            fontFamily: "'Nunito Sans Variable', sans-serif",
+          }}
+        >
+          {name}
+        </span>
+        <span
+          className="text-base"
+          style={{
+            color: THEME.textSecondary,
+            fontFamily: "'Nunito Sans Variable', sans-serif",
+          }}
+        >
+          {showId ? (
+            id
+          ) : (
+            description
+          )}
+        </span>
+      </div>
     </div>
   )
 }
